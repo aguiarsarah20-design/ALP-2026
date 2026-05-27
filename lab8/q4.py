27bd2fbdc289f1540edebe835bc28beb4e8a9a51
aguiarsarah20-design <aguiarsarah20@gmail.com>
2026-05-27 15:34:05 -0300

    Create q4.py

---
+++ lab8/q4.py
@@ -0,0 +1,13 @@
+#catina do if 💕
+
+total = 0
+aberto = 1
+    while aberto == 1:
+        print('cardapio')
+        print("1. Açaí 300ml - R$12")
+        print("2. Mousse - R$6.50")
+        print("3. Salada de frutas - R$10")
+        print("4. Fechar conta")
+        opção= int(input('adicione uma opção:'))
+        if opção == 1
+            
