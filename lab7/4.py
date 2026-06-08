a0a9009f47a5e97f92e746cde076e12deeb0d348
aguiarsarah20-design <aguiarsarah20@gmail.com>
2026-06-08 18:18:02 -0300

    Create 4.py

---
+++ lab7/4.py
@@ -0,0 +1,8 @@
+import random
+chances = 3
+while  chances > 0: 
+    num = input(f"Qual o numero  secreto? Você tem {chances} chances")
+    if numero == num:
+        print("Você acertou o numero")
+        break
+print(random.randint(1, 10))
